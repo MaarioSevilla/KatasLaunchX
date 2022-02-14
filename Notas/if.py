9b036c24ade972affa7ce586328cb62ff7d01a52
MaarioSevilla <oficialmariosevilla@icloud.com>
2022-02-14 11:50:34 -0600

    first commit katas

---
+++ Notas/if.py
@@ -0,0 +1,18 @@
+# Tip de práctica 1: Intenta ejecutarlo en un notebook.
+a = 97
+b = 55
+# test expression / expresión de prueba
+if a < b:
+    # statement to be run / instrucción a ejecutar
+    print(b)
+
+
+
+a = 93
+b = 27
+if a > b:
+    print("a es mayor que b")
+elif a < b:
+    print("a es menor que b")
+else: 
+    print ("a es igual que b")
---
+++ Notas/or-and.py
@@ -0,0 +1,10 @@
+a = 23
+b = 34
+if a == 34 or b == 34:
+    print(a + b)
+
+
+a = 23
+b = 34
+if a == 34 and b == 34:
+    print (a + b)
